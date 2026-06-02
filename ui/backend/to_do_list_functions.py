cde159541a79474ff922fa6fe08878d9b300bb1f
George Zhao <george.zhao@my.bdsc.school.nz>
2026-06-03 09:03:31 +1200

    Added sorting function

--- ui/backend/to_do_list_functions.py
+++ ui/backend/to_do_list_functions.py
@@ -19,12 +19,14 @@ def save_tasks():
     with open(FILE_PATH, "w") as file:
         json.dump(task_list, file)
 
-def add_new_task(title, description):
-    task_list.append({"title": title, "description": description})
+def add_new_task(title, description, priority, date):
+    task_list.append({"title": title, "description": description, "priority": priority, "date": date})
+    # Sort the tasks in order of priority/date
+    sort_tasks()
     save_tasks()
 
-def edit_task(task_id, title, description):
-    task_list[task_id] = {"title": title, "description": description}
+def edit_task(task_id, title, description, priority, date):
+    task_list[task_id] = {"title": title, "description": description, "priority": priority, "date": date}
     save_tasks()
 
 def delete_task(task_id):
@@ -35,8 +37,22 @@ def get_task_info(task_id):
     return task_list[task_id]
 
 def get_shortened_title(task_id):
+    # Get the title for the task and shorten it to at most TITLE_LENGTH
     title = task_list[task_id]["title"]
     shortened_title = title[:TITLE_LENGTH]
     return shortened_title
+
+def sort_tasks():
+    # Create a dictionary that convers between the priority options and their order in numbers (0, 1, 2...)
+    priority_map = {PRIORITY_OPTIONS[i]:i for i in range(len(PRIORITY_OPTIONS))}
+
+    # Sorts the task list. First based on priority, then within the same priority, sort by due date
+    task_list.sort(
+        key=lambda task: (
+            priority_map[task["priority"]],
+            task["date"]
+        )
+    )
+
 
 task_list = access_tasks()
