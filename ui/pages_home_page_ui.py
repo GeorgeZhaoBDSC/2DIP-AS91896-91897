e4a900fa582d0e784bf3c2dc48c807e78dd091c9
George Zhao <george.zhao@my.bdsc.school.nz>
2026-06-15 14:52:44 +1200

    Added buttons to switch between lists.

--- ui/pages_home_page_ui.py
+++ ui/pages_home_page_ui.py
@@ -17,7 +17,7 @@ def create_home_page(system_state):
     # Expandable window: Top row remains the same width, second row expands, starting at height of 400px
     home_page.rowconfigure(1,minsize = 400, weight=1)
 
-    # The starting width of the window columns is 250/500px, and then expands with the ratio of 1:5
+    # The starting width of the window columns is 100/500px, and then expands with the ratio of 1:5
     home_page.columnconfigure(0, minsize = 250, weight=1)
     home_page.columnconfigure(1, minsize = 500, weight=5)
 
@@ -55,17 +55,25 @@ def create_home_page(system_state):
         sticky=""
     )
 
-    # ------------------------------------- Left side bar ------------------------------------
+    # ================================================ Left side bar ================================================
+
+    # ------------------------------------- Add task button ------------------------------------
     left_container = tk.Frame(
         home_page,
         padx=20,
         pady=20,
-        bg="white"
+        bg="white",
     )
+
+    # Stops the elements inside left container from changing its width and stretching it
+    left_container.grid_propagate(False)
+
     left_container.grid(row=1, column=0, sticky="nsew")
 
-    # Adding a nested grid inside the left container for more of the UI
+    # Adding a nested grid inside the left container for more of the UI, configured the rows
+    left_container.grid_rowconfigure(0, weight=1)
     left_container.grid_rowconfigure(1, weight=1)
+    left_container.grid_rowconfigure(2, weight=3)
     left_container.grid_columnconfigure(0, weight=1)
 
     # Button to add a new task, takes user to the page where you enter a new task
@@ -73,8 +81,6 @@ def create_home_page(system_state):
         left_container,
         text="Add new task",
         command=lambda: switch_page(system_state, "add_task"),
-        padx=20,
-        pady=20,
         font=("Times New Roman", 18),
         bg = "#2563EB",
         fg = "black",
@@ -84,8 +90,92 @@ def create_home_page(system_state):
         cursor = "hand1",
     )
 
-    add_task_button.grid(row=0, column=0, sticky="nsew")
+    # Scroll bar with buttons to switch between lists
+    # Padding seperates the add task button from the list buttons
+    add_task_button.grid(row=0, column=0, sticky="nsew", pady=(0, 15))
 
+    # ---------------------------------- List Buttons title ---------------------------------
+
+    # Created a label for the list buttons below
+    list_title = tk.Label(
+        left_container,
+        text="To Do Lists:",
+        font=("Times New Roman", 18),
+        fg="black",
+        bg="white",
+        relief="flat",
+        anchor="center",
+        justify="center"
+    )
+
+    list_title.grid(
+        row=1,
+        column=0,
+        sticky="nsew",
+        padx=10,
+        pady=10,
+    )
+
+    # ------------------------------------- List Buttons ------------------------------------
+
+    # Create container for the buttons
+    list_btn_container = tk.Frame(left_container)
+
+    # Resize the grid areas
+    list_btn_container.rowconfigure(0, weight=1)
+    list_btn_container.columnconfigure(0, weight=1)
+
+    list_btn_container.grid(row=2, column=0, sticky="nsew")
+
+    btn_canvas = tk.Canvas(list_btn_container, bg="white", highlightthickness=0)
+    btn_scrollbar = tk.Scrollbar(
+        list_btn_container,
+        orient="vertical",
+        command=btn_canvas.yview
+    )
+
+    # Create scroll bar
+    btn_canvas.configure(yscrollcommand=btn_scrollbar.set)
+
+    # Assign areas for the buttons and scroll bar
+    btn_canvas.grid(row=0, column=0, sticky="nsew")
+    btn_scrollbar.grid(row=0, column=1, sticky="ns")
+
+    # Create the frame to put the buttons of the scroll bar in
+    btn_frame = tk.Frame(btn_canvas)
+    btn_window = btn_canvas.create_window((0, 0), window=btn_frame, anchor="nw")
+    btn_frame.grid_columnconfigure(0, weight=1)
+
+    def update_btn_scrollregion(event):
+        btn_canvas.configure(scrollregion=btn_canvas.bbox("all"))
+
+    def resize_btn_frame(event):
+        btn_canvas.itemconfig(btn_window, width=event.width)
+
+    # Configure the regions to update when scrolling
+    btn_frame.bind("<Configure>", update_btn_scrollregion)
+    btn_canvas.bind("<Configure>", resize_btn_frame)
+
+    # Create the button for each list
+    def create_button(list_id):
+        button = tk.Button(
+            btn_frame,
+            text=f"hii",
+            padx=10,
+            pady=10,
+            font=TEXT_FONT,
+            bg="lightgrey",
+            fg="black",
+            relief="flat",
+            activebackground="#D6D6D6",
+            bd=0,
+            cursor="hand1",
+        )
+
+        button.grid(row=list_id, column=0, sticky="nsew", pady=5)
+
+
+    # =========================================== Tasks cards (right side) ===========================================
 
     # --------------------------------- Scroll bar Container ---------------------------------
     scroll_container = tk.Frame(home_page)
@@ -224,15 +314,22 @@ def create_home_page(system_state):
 
         parent.grid_columnconfigure(0, weight=1)
 
+
     # Creates a card for every to do list item, and also updates it when any changes are made to the list of tasks
     # So that the new cards also show up
-    def refresh_home_page_cards():
+    def refresh_home_page():
         for widget in card_frame.winfo_children():
             widget.destroy()
 
         for i in range(len(task_list)):
             create_card(card_frame, i, i)
 
-    refresh_home_page_cards()
+        for widget in btn_frame.winfo_children():
+            widget.destroy()
 
-    return home_page, refresh_home_page_cards
+        for i in range(5):
+            create_button(i)
+
+    refresh_home_page()
+
+    return home_page, refresh_home_page
